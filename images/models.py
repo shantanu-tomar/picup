240b538213dd9374c44f0d03ef36d8d2ddf8c5b6
Shantanu Tomar <shantanu.tomar94@gmail.com>
2020-12-30 08:44:59 +0530

    Add Image model to images app

--- images/models.py
+++ images/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+UPLOAD_FOLDER = 'uploads'
+
+
+class Image(models.Model):
+    image = models.ImageField(upload_to=UPLOAD_FOLDER)
+    uploaded_on = models.DateTimeField(auto_now_add=True)
+
+    def get_image_name(self):
+        return self.image.name.replace(f"{UPLOAD_FOLDER}/", '')
